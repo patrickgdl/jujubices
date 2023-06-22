64508888bb58abc279d55f40fd9bf57a9495a26e
Patrick Lima <patrickgdlima@gmail.com>
2023-06-22 18:26:07 -0300

    feat: login route w/ supabase ui

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import DesignEditor from "~/views/DesignEditor"
 import Dashboard from "~/views/Dashboard"
+import Login from "~/views/Login"
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/login" element={<Login />} />
         <Route path="/manage" element={<Dashboard />} />
         <Route path="/" element={<DesignEditor />} />
       </Routes>
---
+++ src/views/Login/Login.tsx
@@ -0,0 +1,73 @@
+import { Auth } from "@supabase/auth-ui-react"
+import { ThemeSupa } from "@supabase/auth-ui-shared"
+import { Session } from "@supabase/supabase-js"
+import { Block } from "baseui/block"
+import { useEffect, useState } from "react"
+import supabase from "~/services/supabase"
+
+export default function App() {
+  const [session, setSession] = useState<Session | null>(null)
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session)
+    })
+
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session)
+    })
+
+    return () => subscription.unsubscribe()
+  }, [])
+
+  if (!session) {
+    return (
+      <Block $style={{ display: "flex", alignItems: "center", justifyContent: "center", width: "100vw" }}>
+        <Auth
+          supabaseClient={supabase}
+          appearance={{
+            theme: ThemeSupa,
+            variables: {
+              default: {
+                colors: {
+                  brand: "black",
+                  brandAccent: "black",
+                },
+              },
+            },
+          }}
+          providers={["google", "facebook"]}
+          localization={{
+            variables: {
+              sign_in: {
+                email_label: "Seu e-mail",
+                password_label: "Sua senha",
+                email_input_placeholder: "Seu e-mail",
+                password_input_placeholder: "Sua senha",
+                button_label: "Entrar",
+                loading_button_label: "Entrando...",
+                social_provider_text: "Logue com {{provider}}",
+              },
+              forgotten_password: {
+                email_label: "Seu e-mail",
+                password_label: "Sua senha",
+                email_input_placeholder: "Seu e-mail",
+                button_label: "Enviar",
+                loading_button_label: "Enviando...",
+                link_text: "Esqueceu sua senha?",
+                confirmation_text: "Um e-mail foi enviado para você",
+              },
+              sign_up: {
+                link_text: "Não tem uma conta? Cadastre-se",
+              },
+            },
+          }}
+        />
+      </Block>
+    )
+  } else {
+    return <div>Logado!</div>
+  }
+}
